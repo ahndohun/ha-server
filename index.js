84fa20a1270f9db8b2844e7bfe63ffb9a28bf54a
ahndohun <ahndohun1@gmail.com>
2019-08-29 16:58:45 +0900

    db 스키마 개선

--- index.js
+++ index.js
@@ -15,11 +15,21 @@ var app = express();
 app.set("port", process.env.PORT || 3000);
 
 app.get("/", function(req, res) {
-  res.send("Root");
+  res.send("hello node");
 });
 
 app.get("/users", function(req, res) {
-  connection.query("SELECT * from data", function(err, rows) {
+  connection.query("SELECT * from users", function(err, rows) {
+    if (err) throw err;
+    res.send(rows);
+  });
+});
+
+app.get("/users/:id", function(req, res) {
+  connection.query(`SELECT * from users WHERE id = ${req.params.id}`, function(
+    err,
+    rows
+  ) {
     if (err) throw err;
     res.send(rows);
   });
